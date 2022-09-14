2243bf32e1b8990d6fcb619049416b01f8699ebb
ASGladchenko <asgladchenko@gmail.com>
2022-09-14 10:54:15 +0300

    Homework 33

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
-let cards = document.querySelector('.items')
-cards.addEventListener('click',(event)=>{
-    if (event.target.closest('.item') && !event.target.closest('.item').classList.contains('active') ) {
-        event.target.closest('.item').classList.add('active')
-        event.target.closest('.items').append(event.target.closest('.item'))
+class Animal {
+    constructor(name) {
+        this.name = name
     }
-})
+
+    sayHi (){
+        alert(`My name is ${this.name}`)
+    }
+}
+class Dog extends Animal{
+    super(name) {
+        this.name = name
+    }
+    sayHello(){
+        alert(`Hello from ${this.name}, I am a Dog`)
+    }
+}
+class Cat extends Animal{
+    super(name) {
+        this.name = name
+    }
+    sayHello(){
+        alert(`Hello from ${this.name}, I am a Cat`)
+    }
+}
