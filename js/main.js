43b9d3ecce36933fd9a1e8e929a74052183659f9
ASGladchenko <asgladchenko@gmail.com>
2022-09-14 13:13:56 +0300

    Homework 33

--- js/main.js
+++ js/main.js
@@ -3,23 +3,58 @@ class Animal {
         this.name = name
     }
 
-    sayHi (){
+    sayHi() {
         alert(`My name is ${this.name}`)
     }
 }
-class Dog extends Animal{
-    super(name) {
-        this.name = name
-    }
-    sayHello(){
+
+class Dog extends Animal {
+    sayHello() {
         alert(`Hello from ${this.name}, I am a Dog`)
     }
 }
-class Cat extends Animal{
-    super(name) {
-        this.name = name
-    }
-    sayHello(){
+
+class Cat extends Animal {
+    sayHello() {
         alert(`Hello from ${this.name}, I am a Cat`)
     }
 }
+
+let pet;
+
+let form = document.querySelector('form')
+
+let input = form.querySelector('input')
+
+let sayDiv = document.querySelector('.hi')
+
+form.addEventListener('click', (event) => {
+    event.preventDefault()
+    if (event.target.tagName !== "INPUT" && !validator(event.target)) {
+        alert('Your pet\'s name must contain at least 3 characters')
+        return
+    }
+    if (event.target.id === "dog") {
+        pet = new Dog(input.value)
+        input.value = ""
+        form.remove()
+    }
+    if (event.target.id === "Cat") {
+        pet = new Cat(input.value)
+        input.value = ""
+        form.remove()
+    }
+
+
+})
+
+sayDiv.addEventListener('click', (event) => {
+    if (event.target.tagName !== "BUTTON") return
+    if (event.target.dataset.pet === "SayHi") pet.sayHi()
+    if (event.target.dataset.pet === "SayHello") pet.sayHello()
+})
+
+function validator(element) {
+    if (element.tagName === "BUTTON" && (input.value !== "" && input.value.length >= 3)) return true
+}
+
